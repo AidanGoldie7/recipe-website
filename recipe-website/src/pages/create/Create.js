24667ff20984661690050a73e94d1346e5cad3b8
aidangoldie7 <40402670@live.napier.ac.uk>
2023-02-20 12:41:05 +0000

    12. Create Recipe Form

--- recipe-website/src/pages/create/Create.js
+++ recipe-website/src/pages/create/Create.js
@@ -1,11 +1,63 @@
+import {useState} from 'react'
+
 //styles 
 import './Create.css'
 
 
 export default function Create() {
+  const [title, setTitle] = useState('')
+  const [method, setMethod] = useState('')
+  const [cookingTime, setCookingTime] = useState('')
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(title, method, cookingTime)
+  }
+
   return (
-    <div>
-        Create
+    <div className='create'>
+        <h2 className='page-title'>Add New Recipe</h2>
+
+
+        <form onSubmit={handleSubmit}>
+
+          <label>
+            <span> Recipe Title:</span>
+            <input 
+            type="text" 
+            onChange={(e) => setTitle(e.target.value)}
+            value={title}
+            required
+            />
+          </label>
+
+          
+
+          <label>
+            <span>Recipe Method:</span>
+            <textarea
+              onChange={(e) => setMethod(e.target.value)} 
+              value={method}
+              required
+            />
+          </label>
+
+
+
+          <label>
+            <span>Cooking Time (minutes):</span>
+            <input 
+              type="number"
+              onChange={(e) => setCookingTime(e.target.value)} 
+              value={cookingTime}
+              required/>
+          </label>
+
+
+          <button className='btn'>Submit</button>
+        </form>
+
     </div>
   )
 }

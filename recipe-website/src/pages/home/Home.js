202ecbbe68f4abe58e6e16a0d96920e19b836134
aidangoldie7 <40402670@live.napier.ac.uk>
2023-02-20 12:08:18 +0000

    8. Recipe Tiles & Animation

--- recipe-website/src/pages/home/Home.js
+++ recipe-website/src/pages/home/Home.js
@@ -3,18 +3,18 @@ import {useFetch} from '../../hooks/useFetch'
 //styles 
 import './Home.css'
 
+//components
+import RecipeList from '../../components/RecipeList'
+
 
 export default function Home() {
   const {data, isPending, error} = useFetch ('http://localhost:3000/recipes')
+  
   return (
     <div className='home'>
       {error && <p className='error'>{error}</p>}
       {isPending && <p className='loading'>Loading...</p>}
-      {data && data.map(recipe => (
-          <h2 key={recipe.id}>{recipe.title}</h2>
-      )
-      )}
-      
+      {data && <RecipeList recipes={data}/>}
     </div>
   )
 }

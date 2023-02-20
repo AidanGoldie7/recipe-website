3f434b1faca1f327d54231d3a50bb8bce0a6197f
aidangoldie7 <40402670@live.napier.ac.uk>
2023-02-20 10:49:39 +0000

    4. Router Pages

--- recipe-website/src/App.js
+++ recipe-website/src/App.js
@@ -1,11 +1,44 @@
+import {BrowserRouter, Switch, Route} from 'react-router-dom'
+
+//page components 
+import Home from './pages/home/Home'
+import Create from './pages/create/Create'
+import Recipe from './pages/recipe/Recipe'
+import Search from './pages/search/Search'
+
+//styles 
 import './App.css'
+
+
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+      <Switch>
+
+        <Route exact path ="/">
+          <Home />
+        </Route>
+
+        <Route exact path ="/create">
+          <Create />
+        </Route>
+
+        <Route exact path ="/search">
+          <Search />
+        </Route>
+
+        <Route exact path ="/recipes/:id">
+          <Recipe />
+        </Route>
+
+      </Switch>
+      </BrowserRouter>
     </div>
   );
 }
+
+
 
 export default App

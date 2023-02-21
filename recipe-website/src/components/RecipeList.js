fe49c474da9ae35d7f6073b07032e184c6fe6c23
aidangoldie7 <40402670@live.napier.ac.uk>
2023-02-21 10:24:38 +0000

    18. No Recipes found message for invalid search

--- recipe-website/src/components/RecipeList.js
+++ recipe-website/src/components/RecipeList.js
@@ -5,6 +5,14 @@ import './RecipeList.css'
 
 
 export default function RecipeList({recipes}) {
+
+  if (recipes.length === 0) {
+    return <div className='error'>No recipes found.</div>
+    
+  }
+
+
+
   return (
     <div className='recipe-list'>
         {recipes.map(recipe => (

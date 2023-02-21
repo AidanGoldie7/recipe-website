34120ac89540ec7c5adefeb623f0175c2197ba52
aidangoldie7 <40402670@live.napier.ac.uk>
2023-02-21 10:21:19 +0000

    17. Search Bar Functionality

--- recipe-website/src/pages/search/Search.js
+++ recipe-website/src/pages/search/Search.js
@@ -1,11 +1,30 @@
+import { useLocation } from 'react-router-dom'
+import {useFetch} from '../../hooks/useFetch'
+
+
 //styles 
 import './Search.css'
 
+//components 
+import RecipeList from '../../components/RecipeList'
+
 
 export default function Search() {
+  const queryString = useLocation().search
+  const queryParams = new URLSearchParams(window.location.search)
+  const query = queryParams.get('query')
+
+
+  const url = 'http://localhost:3000/recipes?q=' + query
+  const { error, isPending, data } = useFetch(url)
+
+
   return (
     <div>
-        Search
+      <h2 className="page-title">Recipes Including "{query}"</h2>
+      {error && <p className="error">{error}</p>}
+      {isPending && <p className="loading">Loading...</p>}
+      {data && <RecipeList recipes={data} />}
     </div>
   )
 }
